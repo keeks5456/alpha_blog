35bb4d778b7253271a4a96c5b300d873652678f3
Akiko Green <agreen@alphaworks.tech>
2022-08-17 11:25:50 -0700

    feat(Users)

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
    @user = User.new
   end
 
+  def create 
+    @user = User.create(user_params)
+    if @user.save 
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you are successfully signed up"
+      redirect_to articles_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
