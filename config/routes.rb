31352ffc75681bda8133e130a55ebcf7b692dfcf
Akiko Green <agreen@alphaworks.tech>
2022-08-17 10:18:49 -0700

    errors(new/ edit and signup bugs deleted)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 
   root "pages#home"
   get 'about' => "pages#about"
-  # get "/articles/:id/edit" => "articles#edit"
   delete "/articles/:id" => "articles#destroy"
   resources :articles, only: [ :index, :show, :new, :create, :edit, :update] #edit
-  # resources :articles #this exposes restful routes for articles
+  get 'signup', to: "users#new"
+  resources :users, except: [:new, :show]
 end
