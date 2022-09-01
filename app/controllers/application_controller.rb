55ab1bd3c4f496c3a77a0bb23c277d4a9174dab1
Akiko Green <agreen@alphaworks.tech>
2022-09-01 15:24:44 -0700

    feat(Users login access)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  
+    
+  helper_method :current_user, :logged_in?
+  #returns the current logged-in user (if any)
+  def current_user
+    if @current_user.nil?
+      @current_user = User.find_by(id:session[:user_id]) 
+    else
+      @current_user
+    end
+      # of we can do @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,19 +4,4 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
-  #returns the current logged-in user (if any)
-  def current_user
-    if @current_user.nil?
-      @current_user = User.find_by(id:session[:user_id]) 
-    else
-      @current_user
-    end
-    # of we can do @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-    def logged_in?
-      !current_user.nil?
-    end
-
 end
