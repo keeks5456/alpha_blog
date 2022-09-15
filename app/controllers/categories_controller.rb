0b1d02f1e034d9a3fabccbc3e86ff1255dba7d37
Akiko Green <agreen@alphaworks.tech>
2022-09-15 14:06:34 -0700

    test(category controller test for new create)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,12 +4,33 @@ class CategoriesController < ApplicationController
   def index 
 
   end
-  
+
   def show
 
   end
 
   def new 
-
+    @category = Category.new
   end
+
+  def create
+   @category = Category.create(category_params)
+  #  @category.user = current_user
+    if @category.save
+      flash[:notice] = "Category was created successfully!"
+      # redirect_to@category    //can use either or
+      redirect_to category_path(@category)
+    else
+      # create a flash warning for user to see when invalid input
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+
 end
