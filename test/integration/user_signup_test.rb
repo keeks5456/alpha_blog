784b6a1dbf6932369415239c5322cc84bc59690a
Akiko Green <agreen@alphaworks.tech>
2022-09-22 14:59:52 -0700

    uncomment out code

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -2,16 +2,16 @@ require "test_helper"
 
 class UserSignupTest < ActionDispatch::IntegrationTest
 
-  # test "valid signup information" do 
-  #   get signup_path
-  #   assert_response :success 
-  #   assert_difference 'User.count', 1 do 
-  #     post users_path, params: { user: {  username: "jake", email: "user@invalid.com", password: "password" } }
-  #     assert_response :redirect
-  #   end
-  #   assert_response :redirect
-  #   follow_redirect!
-  # end
+  test "valid signup information" do 
+    get signup_path
+    assert_response :success 
+    assert_difference 'User.count', 1 do 
+      post users_path, params: { user: {  username: "jake", email: "user@invalid.com", password: "password" } }
+      assert_response :redirect
+    end
+    assert_response :redirect
+    follow_redirect!
+  end
 
   test "invalid signup information" do
     get signup_path
